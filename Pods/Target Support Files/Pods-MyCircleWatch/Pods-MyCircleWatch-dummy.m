4b34b4ea0c50bf7942202b0993c46ef458f882ea
xiaozhangfei <951921265@qq.com>
2016-04-01 10:41:20 +0800

    使用jenkins自动化编译，上传pods文件夹

---
+++ Pods/Target Support Files/Pods-MyCircleWatch/Pods-MyCircleWatch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyCircleWatch : NSObject
+@end
+@implementation PodsDummy_Pods_MyCircleWatch
+@end
---
+++ Pods/Target Support Files/VEVerbalExpressions/VEVerbalExpressions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VEVerbalExpressions : NSObject
+@end
+@implementation PodsDummy_VEVerbalExpressions
+@end
